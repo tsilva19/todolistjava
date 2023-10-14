5428d4a47a9f871be9bf465b3914db77fe663895
Thiago <thiagoalves060@gmail.com>
2023-10-14 13:22:44 -0300

    segurança

--- src/main/java/br/com/tsilva/todolist/user/UserController.java
+++ src/main/java/br/com/tsilva/todolist/user/UserController.java
@@ -1,6 +1,10 @@
 package br.com.tsilva.todolist.user;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,9 +18,19 @@ public class UserController {
     private IUserRepository userRepository;
 
     @PostMapping("/")
-    public UserModel create(@RequestBody UserModel userModel){
+    public ResponseEntity create(@RequestBody UserModel userModel){
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+        if(user != null){
+            System.out.println("Usuario já Existe");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario já existe");
+        }
+
+        var passHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passHashred);
+
         var userCreated = this.userRepository.save(userModel);
-        return userCreated;
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
         
     }
     
