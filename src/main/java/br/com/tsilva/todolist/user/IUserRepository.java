1fa210e3091129639c2d496ea81c2fd25f802dab
Thiago <thiagoalves060@gmail.com>
2023-10-14 09:47:47 -0300

    todolist

---
+++ src/main/java/br/com/tsilva/todolist/user/IUserRepository.java
@@ -0,0 +1,8 @@
+package br.com.tsilva.todolist.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID> {
+}
